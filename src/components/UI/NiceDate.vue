94c95c455929ec7deb02011550e3f7b95561b2a9
Simon Dann <simon.dann@halesway.co.uk>
2017-02-09 16:41:36 +0000

    :sparkles: added nice date output

---
+++ src/components/UI/NiceDate.vue
@@ -0,0 +1,17 @@
+<template>
+    <span>{{ pretty }}</span>
+</template>
+
+<script>
+    import Moment from 'moment'
+    export default {
+        props: {
+            date: { required: true },
+            format: { default: 'MMMM Do YYYY, h:mm:ss a', type: String, required: false }
+        },
+        computed: {
+            now () { return Moment() },
+            pretty () { return Moment(this.date).format(this.format) }
+        }
+    }
+</script>
